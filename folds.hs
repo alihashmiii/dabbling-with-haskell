ed302aa98791da55972b4965eb9b07ffe6b841f2
Ali Hashmi <ah946@cam.ac.uk>
2017-08-22 11:53:47 +0200

    Update folds.hs

--- folds.hs
+++ folds.hs
@@ -1,1 +1,10 @@
+-- fold with lambda -- 
+sum' :: (Num a) => [a] -> a
+sum' xs = foldr (\x acc -> x + acc) 0 xs
 
+-- because of currying the above function can be re-written as 
+sum'' xs = foldr (+) 0 xs
+
+-- constructing `elem` using folds 
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc n -> if n == y then True else acc) False ys
